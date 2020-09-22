e6ecce84da7d364c550fa2a982aab6ac01621839
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-21 21:20:39 -0400

    Correction

--- ProjectAnthonyBeauregard/Location.cpp
+++ ProjectAnthonyBeauregard/Location.cpp
@@ -1,6 +1,6 @@
-//But : Écrire un programme pour calculer le montant qu’un individu devra débourser pour faire un voyage dans une
-//automobile louée.L’algorithme tient compte du kilométrage parcouru et de la durée du voyage qui sont
-//soumises par le requérant.
+//But : Ã‰crire un programme pour calculer le montant quâ€™un individu devra dÃ©bourser pour faire un voyage dans une
+//automobile louÃ©e.Lâ€™algorithme tient compte du kilomÃ©trage parcouru et de la durÃ©e du voyage qui sont
+//soumises par le requÃ©rant.
 
 //Auteur: Anthony Beauregard
 //Date:14/09/2020
@@ -12,68 +12,77 @@ int main() {
 
 
 	setlocale(LC_ALL, "");
-		//Déclaration des variables
+		//DÃ©claration des variables
 	
-	int jourut;    //Le nombre de jour que l'utilisateur compte avoir le véhicule
-	int jourargent = 45; //Le cout du véhicule par jour
+	int jourut;    //Le nombre de jour que l'utilisateur compte avoir le vÃ©hicule
+	int jourargent = 45; //Le cout du vÃ©hicule par jour
 	
-	int jourkm; //Nombre de KM utiliser par jour
+	int jourkm; //FR : Nombre de KM utiliser par jour
 	int gratuiskm = -250;   // Le nombre de km gratuis par jour
-	int km = 0.05;       // le cout de chaque km si l'utilisateur dépasse le 250km offert
 	
-	int essence = 7.6 / 100;  //consommation d'essence du véhicule
-	//  prixessence = 1.25; //prix de l'essence  ((Je le met en commentaire parce que je ne pense pas l'utiliser comme variable, mais je préfère le précisé quand même.
+	// KM : Tu ne peux pas mettre une valeur dÃ©cimale dans un entier
+	int km = 0.05;       // le cout de chaque km si l'utilisateur dÃ©passe le 250km offert
 	
-	//On demande a l'utilisateur le nombre de jour qu'il veut avoir le véhicule, le nombre de km par jours
+	// KM : MÃªme chose ici, tu vas perdre la virgule. Il faut un float
+	int essence = 7.6 / 100;  //consommation d'essence du vÃ©hicule
+	//  prixessence = 1.25; //prix de l'essence  ((Je le met en commentaire parce que je ne pense pas l'utiliser comme variable, mais je prÃ©fÃ¨re le prÃ©cisÃ© quand mÃªme.
+	
+	//On demande a l'utilisateur le nombre de jour qu'il veut avoir le vÃ©hicule, le nombre de km par jours
 
-	std::cout << "Veuillez entrer le nombre de jours de location désiré : ";
+	std::cout << "Veuillez entrer le nombre de jours de location dÃ©sirÃ© : ";
 	std::cin >> jourut;
 	//On demande combien de KM il veut parcourir chaques jours
 
-	std::cout << "Veuillez entrer le nombre de km à parcourir par jour : ";
+	std::cout << "Veuillez entrer le nombre de km Ã  parcourir par jour : ";
 	std::cin >> jourkm;
 
 
 
-	//Le programme va éxécuter plusieurs calcul, dont la consommation d'essence, le cout des km, cout des jours, etc.
+	//FR : Le programme va Ã©xÃ©cuter plusieurs calcul, dont la consommation d'essence, le cout des km, cout des jours, etc.
 
-	std::cout << "Le coût de la location du véhicule sera de " << (jourut * jourargent) << " $."<< std::endl;   //Celui là marche.
+	std::cout << "Le coÃ»t de la location du vÃ©hicule sera de " << (jourut * jourargent) << " $."<< std::endl;   //Celui lÃ  marche.
 	
 	
 	/*
 	
-	std::cout << (jourkm - gratuiskm) * 0.05;     //Bon , ça marche pas. Ça me donne un nombre qui ne fait pas de sens, alors je vais passer a
-																												// l'autre et garder celui là en commentaire pour pas faire bugger le programme
-																												*/
+	std::cout << (jourkm - gratuiskm) * 0.05;     //Bon , Ã§a marche pas. Ã‡a me donne un nombre qui ne fait pas de sens, alors je vais passer a
+																												// l'autre et garder celui lÃ  en commentaire pour pas faire bugger le programme
+	// KM : C'est Ã  cause du int Ã  la place du float																											*/
 
 
 	//On va essayer le cout de l'essence
 
-	//std::cout << (jourkm * essence) * 1.25;   Bon, apparament je ne sais pas comment faire des priorités d'opération ou dumoin des opération a plus d'un calcul alors je vais attendre en classe.
+	//std::cout << (jourkm * essence) * 1.25;   FR : Bon, apparament je ne sais pas comment faire des prioritÃ©s d'opÃ©ration ou dumoin des opÃ©ration a plus d'un calcul alors je vais attendre en classe.
 
 
 
 	/*
 	Plan de test
 
-	Jours                Résulats
+	Jours                RÃ©sulats
 	8                       360
 	4						180
 	2						90
 	
-	
-	
-	
-	
-	
 	*/
-
-
-
-
-
-
 
 
 	return 0;
 }
+
+/*
+â€¢	La solution est bien nommÃ©e et comporte un projet 5/ 5
+â€¢	Les fichiers sources contiennent l'identification complÃ¨te du programme et de son auteur	5/5
+â€¢	Le code source est sans erreur de compilation 8/ 15		// Beaucoup de code en commentaire
+â€¢	Le programme est fonctionnel 5/ 20
+â€¢	Le programme passe le plan de tests 8/ 20
+â€¢	Le code source est largement commentÃ© 20 / 20		// KM : C'est parfati que tu aies laissÃ© une trace de ton raisonnement
+â€¢	La solution est dÃ©posÃ©e dans LEA et dans GitHub.L'adresse est envoyÃ©e via Outlook	10/15
+Dans LEA, il y a seulement le fichier .sln
+
+
+
+Note 53/100
+Dommage que tu ne m'as pas demandÃ© de l'aide car juste Ã  cause du int Ã  la place d'un float et tu aurais pu amÃ©liorer le fonctionnement du programme.
+Ne te dÃ©courage pas, tu avais le raisonnement.
+*/
